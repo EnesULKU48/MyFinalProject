87217243a4526d436412f2bb75e541c39f8af945
Enes ÜLKÜ <enesulku4834@gmail.com>
2021-01-31 00:10:00 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //public, diğer katmanlarda erişebilsin diye
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+
+        public int CategoryId { get; set; }
+        
+        public string ProductName { get; set; }
+
+        public short UnitsInStock { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+    }
+}
