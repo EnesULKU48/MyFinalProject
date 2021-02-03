5aecdf8ba383ad238e6dff4b566847ec711e8fe5
Enes ÜLKÜ <enesulku4834@gmail.com>
2021-02-03 23:47:29 +0300

    03.02.2021 ders sonu

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint - kısıtlama
+    //class : refereans tip olablir
+    //ya IEntity olabilir yada implemente olmuş bir class gelebilir
+    //new() new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class, IEntity, new ()
+    {
+        //null demek filtre vermeyebileceğimiz anlamına gelir
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+
+        public void Add(Product entity)
+        {
+            //işi bitince bellekten atar ve daha performanslı hale gelir
+            //IDisposable pattern implemntation of C#
+            using (NorthwindContext context =new NorthwindContext())
+            {
+                //state = durum
+                var addedEntity = context.Entry(entity);//referansı yakala
+                addedEntity.State = EntityState.Added;//o aslında eklenecek bir nesne
+                context.SaveChanges();//ve şimdi ekle
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                //filtre null mı evet ise tümünü getir : değilse filtelenmiş getir
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)//tek data getirecekti
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+    }
+}
